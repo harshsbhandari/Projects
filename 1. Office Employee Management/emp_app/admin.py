f6d17c828effc64902f519671cde847b9d289aee
harshsbhandari <73375087+harshsbhandari@users.noreply.github.com>
2022-12-01 10:31:31 +0530

    Delete 1. Office Employee Management directory

--- 1. Office Employee Management/emp_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from emp_app.models import Department, Employee, Role
-
-# Register your models here.
-
-admin.site.register(Role)
-admin.site.register(Department)
-admin.site.register(Employee)
